09e95a16369e0c496ca54399e2613df44e334c07
denisjunior <denisdalmeida04@gmail.com>
2021-10-11 16:34:58 +0200

    action with slqserver

--- tests/Fibonacci.Tests/UnitTest1.cs
+++ tests/Fibonacci.Tests/UnitTest1.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using Xunit;
 
 namespace Fibonacci.Tests
@@ -9,8 +11,15 @@ namespace Fibonacci.Tests
         [Fact]
         public async Task Executeshouldreturnanumber()
         {
-            var result = await Compute.ExecuteAsync(new[] {"8"});
-            Assert.Equal(21, result[0]);
+            var builder = new DbContextOptionsBuilder<FibonacciDataContext>(); 
+            var dataBaseName = Guid.NewGuid().ToString(); builder.UseInMemoryDatabase(dataBaseName);  
+            var options = builder.Options; var fibonacciDataContext = new FibonacciDataContext(options); 
+            await fibonacciDataContext.Database.EnsureCreatedAsync();
+
+            var result = await new Fibonacci.Compute(fibonacciDataContext).ExecuteAsync();
+            Assert.Equal(1, result.Count);
+            Assert.Equal(8, result[0]);
+
 
         }
     }
